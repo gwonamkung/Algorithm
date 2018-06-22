a756899effd06e04af4ac4f57eebf9498cbd2e7a
gwonamkung <rnjs4rk@gmail.com>
2018-06-22 18:24:06 +0900

    .

--- ExpertAcademy/src/level2/어디에단어가들어갈수있을까.java
+++ ExpertAcademy/src/level2/어디에단어가들어갈수있을까.java
@@ -1,5 +1,20 @@
 package level2;
 
-public class 어디에단어가들어갈수있을까 {
+import java.util.Scanner;
 
+public class 어디에단어가들어갈수있을까 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int test_case=0; test_case<n; test_case++) {
+			int size = sc.nextInt();
+			int len = sc.nextInt();
+			int[][] arr = new int[size][size];
+			for (int i=0; i<size; i++) {
+				for (int j=0; j<size; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+		}
+	}
 }
